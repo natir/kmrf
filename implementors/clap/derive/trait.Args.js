75cd309fce4f0bc15ea19da5a54f7caf638a4b6e
natir <natir@users.noreply.github.com>
2022-01-28 13:38:50 +0000

    deploy: f477da45c69bb56f119054fd24948407d30a251b

--- implementors/clap/derive/trait.Args.js
+++ implementors/clap/derive/trait.Args.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["kmrf"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/clap/3.0.0-beta.5/clap/derive/trait.Args.html\" title=\"trait clap::derive::Args\">Args</a> for <a class=\"struct\" href=\"kmrf/cli/struct.Command.html\" title=\"struct kmrf::cli::Command\">Command</a>","synthetic":false,"types":["kmrf::cli::Command"]}];
+implementors["kmrf"] = [{"text":"impl Args for <a class=\"struct\" href=\"kmrf/cli/struct.Command.html\" title=\"struct kmrf::cli::Command\">Command</a>","synthetic":false,"types":["kmrf::cli::Command"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
